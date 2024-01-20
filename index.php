a9fb2ab4768fc06f569dd6283e64a85903bf95ac
mahanprinceofficial <149046543+mahanprinceofficial@users.noreply.github.com>
2024-01-20 13:30:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<<head>
+<title>Register</title>
+</head>
+<body>
+<center>
+<h1> <u>Welcome<?php echo$_POST["name"];?>
+Your Personal Details are</u></h1><br>
+Name:<?php echo$_POST["name"];?><br>
+Email:<?php echo$_POST["email"];?><br>
+Password:<?php echo$_POST["pwd"];?><br>
+Age:<?php echo$_POST["age"];?><br>
+Gender:<?php echo$_POST["gender"];?><br>
+</center>
+</body>
+</html>
